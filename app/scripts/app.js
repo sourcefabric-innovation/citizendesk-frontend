26bec1a4b8db9bc1447a282753bd7703966bafeb
danse <f.occhipinti@gmail.com>
2014-04-04 17:09:51 +0200

    can save and read a report!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,22 @@
 'use strict';
 
 angular.module('citizenfrontApp', [
-    'ngRoute'
-  ])
+  'ngRoute',
+  'ngResource'
+])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/new-report', {
+        templateUrl: 'views/new-report.html',
+        controller: 'NewReportCtrl'
+      })
+      .when('/list-reports', {
+        templateUrl: 'views/list-reports.html',
+        controller: 'ReportListCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- karma.conf.js
+++ karma.conf.js
@@ -13,9 +13,11 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
+      'test/globals.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
