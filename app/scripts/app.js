99207508a5c467b824c28f0c78d0609f56255f38
danse <f.occhipinti@gmail.com>
2014-04-04 11:53:10 +0200

    yeoman generated

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('citizenfrontApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
