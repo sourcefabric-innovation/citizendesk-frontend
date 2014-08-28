117ba271b4c0521fc4d8ea7e82ef129aac9489b2
danse <f.occhipinti@gmail.com>
2014-08-28 18:25:49 +0200

    add loading indicator, closes #26

--- app/scripts/controllers/new-twitter-search.js
+++ app/scripts/controllers/new-twitter-search.js
@@ -3,6 +3,7 @@
 angular.module('citizendeskFrontendApp')
   .controller('NewTwitterSearchCtrl', ['TwitterSearches', '$scope', '$location', function (TwitterSearches, $scope, $location) {
     $scope.submit = function() {
+      $scope.loading = true;
       TwitterSearches
         .create($scope.terms)
         .then(function (id) {
