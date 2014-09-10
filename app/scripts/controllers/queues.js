5255a3ceb61f9ef5fbc510b23a4bf5f23c7e2714
danse <f.occhipinti@gmail.com>
2014-09-10 16:42:07 +0200

    default collapsed twitter searches in the queues page

--- app/scripts/controllers/queues.js
+++ app/scripts/controllers/queues.js
@@ -3,6 +3,7 @@
 angular.module('citizendeskFrontendApp')
   .controller('QueuesCtrl', function ($scope, TwitterSearches, Monitors, session) {
     $scope.session = session;
+    $scope.collapseSearches = true;
     TwitterSearches.promise.then(function(searches) {
       $scope.searches = searches;
     });
