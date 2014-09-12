c4384fbc6b9effa47d2406d6955d593ed820fbab
danse <f.occhipinti@gmail.com>
2014-09-12 14:03:56 +0200

    do not show reports that fail linking in monitor, closes #51

--- app/scripts/controllers/monitor.js
+++ app/scripts/controllers/monitor.js
@@ -3,6 +3,7 @@
 
 angular.module('citizendeskFrontendApp')
   .controller('MonitorCtrl', function ($scope, $routeParams, api, Monitors, QueueSelection, linkTweetEntities, AliasesInLists, Raven) {
+    $scope.reports = [];
     api.reports
       .query({
         when: JSON.stringify({
@@ -11,14 +12,18 @@ angular.module('citizendeskFrontendApp')
         sort:'[("produced", -1)]'
       })
       .then(function(response) {
-        $scope.reports = response._items;
-        $scope.reports.forEach(function(report) {
+        response._items.forEach(function(report) {
           try {
             report.linkedText = linkTweetEntities(report);
+            AliasesInLists.embedAuthorAlias(report);
+            // reports that throw an exception above in
+            // `linkTweetEntities` will not be added to the
+            // scope. this is a workaround for a weird issue with the
+            // Eve interface, shortly described in issue #51
+            $scope.reports.push(report);
           } catch (e) {
             Raven.raven.captureException(e);
           }
-          AliasesInLists.embedAuthorAlias(report);
         });
       });
     Monitors.getById($routeParams.id)
