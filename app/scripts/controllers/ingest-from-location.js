807d391a0682f76b25d5cb46585dd11f1cd06d4d
danse <f.occhipinti@gmail.com>
2014-09-22 11:50:46 +0200

    show a confirmation message to the user on web link ingestion

--- app/scripts/controllers/ingest-from-location.js
+++ app/scripts/controllers/ingest-from-location.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('citizendeskFrontendApp')
-  .controller('IngestFromLocationCtrl', function ($scope, $http, config, $window, Report, session) {
+  .controller('IngestFromLocationCtrl', function ($scope, $http, config, $window, Report, session, $timeout) {
+    var messageTimeout;
     $scope.cancel = function() {
       $window.history.back();
     };
+    function successMessage() {
+      // when the `repeat` flag is set and the user is adding several
+      // links one after each other, we do not want that the success
+      // of a previous action hides the success of the next
+      // one. not a big deal anyway
+      if (messageTimeout) {
+        $timeout.cancel(messageTimeout);
+      }
+      $scope.showConfirm = true;
+      messageTimeout = $timeout(function() {
+        $scope.showConfirm = false;
+      }, 2000);
+      return messageTimeout;
+    }
     $scope.submit = function() {
       $http
         .post(config.server.url + 'proxy/ingest_from_location/', {
@@ -14,7 +29,11 @@ angular.module('citizendeskFrontendApp')
         .then(function() {
           if ($scope.repeat) {
             $scope.location = '';
-          } else {
+          }
+          return successMessage();
+        })
+        .then(function() {
+          if (!$scope.repeat) {
             $window.history.back();
           }
         });
--- test/spec/controllers/ingest-from-location.js
+++ test/spec/controllers/ingest-from-location.js
@@ -12,14 +12,16 @@ describe('Controller: IngestFromLocationCtrl', function () {
       $httpBackend,
       session = {
         identity: angular.copy(mocks.auth.success)
-      };
+      },
+      $timeout;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, _api_, _$window_, _$httpBackend_) {
+  beforeEach(inject(function ($controller, $rootScope, _api_, _$window_, _$httpBackend_, _$timeout_) {
     api = _api_;
     $window = _$window_;
     spyOn($window.history, 'back');
     $httpBackend = _$httpBackend_;
+    $timeout = _$timeout_;
     
     scope = $rootScope.$new();
     IngestFromLocationCtrl = $controller('IngestFromLocationCtrl', {
@@ -36,12 +38,25 @@ describe('Controller: IngestFromLocationCtrl', function () {
     beforeEach(function() {
       scope.location = 'http://www.example.com';
       scope.submit();
-    });
-    it('calls the relevant interface', function() {
       $httpBackend
         .expectPOST(globals.root + 'proxy/ingest_from_location/')
         .respond();
+    });
+    it('calls the relevant interface', function() {
       $httpBackend.verifyNoOutstandingExpectation();
+    });
+    describe('on response', function() {
+      beforeEach(function() {
+        $httpBackend.flush();
+      });
+      it('shows a message to the user', function() {
+        expect(scope.showConfirm).toBeTruthy();
+      });
+      it('changes the page after the timeout', function() {
+        $timeout.flush();
+        expect(scope.showConfirm).toBeFalsy();
+        expect($window.history.back).toHaveBeenCalled();
+      });
     });
   });
 });
