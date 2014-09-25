8312e092cfbb3b651a1b8cbdc4cfa7ec24ac5e53
danse <f.occhipinti@gmail.com>
2014-09-25 16:24:37 +0200

    reset description on page change, closes #65

--- app/scripts/services/queue-selection.js
+++ app/scripts/services/queue-selection.js
@@ -6,7 +6,16 @@
  to show a description for the currently displayed queue in the queue
  selection section
 
+ eventually i added the description reset on route change, which
+ prevents showing the old description on a different page. see
+ https://github.com/sourcefabric-innovation/citizendesk-frontend/issues/65
+
  */
 
 angular.module('citizendeskFrontendApp')
-  .service('QueueSelection', function QueueSelection() {});
+  .service('QueueSelection', function ($rootScope) {
+    var service = this;
+    $rootScope.$on('$locationChangeSuccess', function() {
+      delete service.description;
+    });
+  });
