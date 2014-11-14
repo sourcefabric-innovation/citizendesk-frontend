31e2326d7c05d38d2c2f184b9976a0eb315a73a2
Pete Haughie <speedwolf@gmail.com>
2014-11-14 17:45:46 +0000

    I want to see published elements too.

--- app/scripts/controllers/assigned-to-me.js
+++ app/scripts/controllers/assigned-to-me.js
@@ -19,7 +19,7 @@ angular.module('citizendeskFrontendApp')
               }, {
                 status: {$exists: false}
               }, {
-                'coverages.published': { $size: 0 }
+                'coverages.published': { $size: 1 }
               }]
             }]
           }),
