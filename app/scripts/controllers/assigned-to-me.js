12f138ab18a9b0f85e7dde4766b79896a2dbc29f
Pete Haughie <speedwolf@gmail.com>
2014-11-18 17:38:29 +0000

    Filtering is handled by the view.

--- app/scripts/controllers/assigned-to-me.js
+++ app/scripts/controllers/assigned-to-me.js
@@ -19,6 +19,20 @@ angular.module('citizendeskFrontendApp')
               }, {
                 status: {$exists: false}
               }, {
+                verified: '',
+              }, {
+                verified: 'verified'
+              }, {
+                type: ''
+              }, {
+                type: 'tweet'
+              }, {
+                type: 'sms'
+              }, {
+                type: 'other'
+              }, {
+                'coverages.published': { $size: 0 }
+              }, {
                 'coverages.published': { $size: 1 }
               }]
             }]
@@ -38,77 +52,34 @@ angular.module('citizendeskFrontendApp')
     .then(function() {
       $scope.loading = false;
     });
-    $scope.$watch('filterResults', function (oldValue) {
-      for (var i in $scope.reports) {
-        if (oldValue === 'newest'){
-          // order by date
-          // is default
-        } else {
-          // order by activity - to be decided / last changed?
-        }
-        i++;
-      }
-    });
+    // array options in the verified status select
+    $scope.filterOptArr = [
+      {'value': 'newest', 'text': 'Newest'},
+      {'value': 'active', 'text': 'Most Active'}
+    ];
+    $scope.$watch('filterResults');
     // array options in the verified status select
     $scope.typeOptArr = [
-      {'value': 'all', 'text': 'All'},
+      {'value': 0, 'text': 'All'},
       {'value': 'tweet', 'text': 'Tweet'},
       {'value': 'sms', 'text': 'SMS'},
       {'value': 'other', 'text': 'Other'}
     ];
-    $scope.$watch('typeFilter', function (oldValue) {
-      for (var i in $scope.reports) {
-        var type = $scope.reports[i].feed_type;
-        if (oldValue === 'sms' && type === 'sms'){
-          // show me the sms
-        }
-        if (oldValue === 'tweet' && type === 'tweet'){
-          // show me the tweets
-        }
-        if (oldValue === 'other' && type === undefined){
-          // show me the undefined
-        }
-        if (oldValue === 'all'){
-          // show me everything
-        }
-        i++;
-      }
-    });
+    $scope.$watch('typeFilter');
     // array options in the verified status select
     $scope.verOptArr = [
-      {'value': 'all', 'text': 'All'},
-      {'value': 'ver-y', 'text': 'Verified'},
-      {'value': 'ver-n', 'text': 'Unverified'}
+      {'value': 0, 'text': 'All'},
+      {'value': 'verified', 'text': 'Verified'},
+      {'value': null, 'text': 'Unverified'}
     ];
-    $scope.$watch('verifiedStatus', function (oldValue) {
-      if (oldValue === 'ver-y') {
-        // show me the verifed posts
-        alterResults(true);
-      }
-      if (oldValue === 'ver-n') {
-        // show me the unverified
-      }
-      if (oldValue === 'all') {
-        // show me everything - default
-      }
-    });
+    $scope.$watch('verifiedStatus');
     // array of options in the published status select
     $scope.pubOptArr = [
-      {'value': 'all', 'text': 'All'},
-      {'value': 'pub-y', 'text': 'Published'},
-      {'value': 'pub-n', 'text': 'Not Published'}
+      {'value': 0, 'text': 'All'},
+      {'value': 1, 'text': 'Published'},
+      {'value': 2, 'text': 'Not Published'}
     ];
-    $scope.$watch('publishedStatus', function (oldValue) {
-      if (oldValue === 'pub-y'){
-        // show me the unpublished elements
-      }
-      if (oldValue === 'pub-n'){
-        // show me the published elements
-      }
-      if (oldValue === 'all'){
-        // show me everything
-      }
-    });
+    $scope.$watch('publishedStatus');
     $scope.dismiss = Report.getDismiss($scope.disabled, function(report) {
       lodash.remove($scope.reports, function(candidate) {
         return candidate._id === report._id;
